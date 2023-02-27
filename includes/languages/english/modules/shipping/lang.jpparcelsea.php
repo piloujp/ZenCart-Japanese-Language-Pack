dafe3b31167b1a29243ec6222e6ac9db3a320142
piloujp <117885587+piloujp@users.noreply.github.com>
2023-02-27 15:14:32 +0900

    International Parcels shipping modules
    Added Japanese Post shipping modules 'International Parcels Air' and 'Surface'.
    With this new addition every Japanese Post international services for parcels with tracking are available. SAL system has been abandonned since Covid19 and 'Small Packets' services that do not offer tracking are more or less replaced by ePackets.

---
+++ includes/languages/english/modules/shipping/lang.jpparcelsea.php
@@ -0,0 +1,8 @@
+<?php
+$define = [
+	'MODULE_SHIPPING_JPPARCELSEA_TEXT_TITLE' =>        'International Parcels Surface',
+	'MODULE_SHIPPING_JPPARCELSEA_TEXT_DESCRIPTION' =>  'International Parcels Surface (2-3 months)',
+	'MODULE_SHIPPING_JPPARCELSEA_TEXT_WAY_NORMAL' =>   'International Parcels Surface (2-3 months) - Some destinations are not available. You can check <a href="https://www.post.japanpost.jp/int/information/overview_en.html" target="_blank">here</a>.</BR>For EU and UK customers there is a minimal order of 22 000&yen; (not including shipping or fees, respectively 150&euro; and 135&#163;)!',
+	'MODULE_SHIPPING_JPPARCELSEA_TEXT_NOTAVAILABLE' => 'Not available for this country.',
+];
+return $define;
