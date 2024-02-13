6b01d64d8bf9837d636441db08218caf928848b0
piloujp <117885587+piloujp@users.noreply.github.com>
2024-02-13 14:53:34 +0900

    Deleted shipping module 'International ePack'

--- includes/languages/english/modules/shipping/lang.jpparcelepack.php
+++
@@ -1,8 +0,0 @@
-<?php
-$define = [
-	'MODULE_SHIPPING_JPPARCELEPACK_TEXT_TITLE' =>        'International ePacket',
-	'MODULE_SHIPPING_JPPARCELEPACK_TEXT_DESCRIPTION' =>  'Japanese Registered Air Small Packet (ePacket 6-13 days)',
-	'MODULE_SHIPPING_JPPARCELEPACK_TEXT_WAY_NORMAL' =>   'International ePacket (6-13 days) - For EU and UK customers there is a minimal order of 22 000&yen; (not including shipping or fees, respectively 150&euro; and 135&#163;)!',
-	'MODULE_SHIPPING_JPPARCELEPACK_TEXT_NOTAVAILABLE' => 'Not available for this country.',
-];
-return $define;
--- includes/languages/japanese/modules/shipping/lang.jpparcelepack.php
+++
@@ -1,8 +0,0 @@
-<?php
-$define = [
-	'MODULE_SHIPPING_JPPARCELEPACK_TEXT_TITLE' =>        '国際eパケット',
-	'MODULE_SHIPPING_JPPARCELEPACK_TEXT_DESCRIPTION' =>  '国際eパケットによる配送料金（eパケット６~１３日）',
-	'MODULE_SHIPPING_JPPARCELEPACK_TEXT_WAY_NORMAL' =>   'eパケット（６~１３日）',
-	'MODULE_SHIPPING_JPPARCELEPACK_TEXT_NOTAVAILABLE' => '要求のあったサービスは、選択された地域間では提供されません。',
-];
-return $define;
