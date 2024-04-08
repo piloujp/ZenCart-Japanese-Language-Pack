e2064af0fcd9c1a27621274c7ce05c4a10028b7a
piloujp <117885587+piloujp@users.noreply.github.com>
2024-04-09 00:03:13 +0900

    Update to ZC V2.0.0

--- sql/mysql_upgrade_zencart_200200.sql
+++ sql/mysql_upgrade_zencart_200200.sql
@@ -5,7 +5,7 @@
 # * @copyright Copyright 2003-2024 Zen Cart Development Team
 # * @copyright Portions Copyright 2003 osCommerce
 # * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
-# * @version $Id: pilou2/piloujp 2024 Modified in v2.0.0-beta1 $
+# * @version $Id: pilou2/piloujp 2024 Modified in v2.0.0 $
 #
 
 ############ IMPORTANT INSTRUCTIONS ###############
