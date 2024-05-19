5f6e212784ecfe87c871e9731871bd759a555185
piloujp <117885587+piloujp@users.noreply.github.com>
2024-05-20 01:39:57 +0900

    Update lang.meta_tags.php

--- includes/languages/japanese/lang.meta_tags.php
+++ includes/languages/japanese/lang.meta_tags.php
@@ -1,8 +1,8 @@
 <?php
 $define = [
-    'TITLE' => 'Zen Cart [日本語版] :',
-    'SITE_TAGLINE' => 'The Art of E-commerce',
-    'CUSTOM_KEYWORDS' => 'ecommerce, open source, shop, online shopping, store',
+    'TITLE' => 'Zen Cart「日本語版」',
+    'SITE_TAGLINE' => '電子商取引の芸術',
+    'CUSTOM_KEYWORDS' => 'ECサイト,電子商取引,Eコマース,オープンソース,ショップ,オンラインショップ,ネットショップ,オンラインショッピング,ストア',
     'HOME_PAGE_META_DESCRIPTION' => '',
     'HOME_PAGE_META_KEYWORDS' => '',
     'HOME_PAGE_TITLE' => '',
