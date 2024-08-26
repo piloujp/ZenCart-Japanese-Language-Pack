37f4ac3001959c6d1c560fbde65e7ddb2520b43a
piloujp <117885587+piloujp@users.noreply.github.com>
2024-08-27 04:55:45 +0900

    Set Japanese admin by default
    Set Japanese admin by default
    changed folder structure
    updated installer
    update status order

---
+++ FILES_to_INSTALL/includes/languages/japanese/lang.order_status.php
@@ -0,0 +1,43 @@
+<?php
+$define = [
+    'NAVBAR_TITLE' => '状況',
+    'NAVBAR_TITLE_1' => 'マイページ',
+
+    'HEADING_TITLE' => '注文情報の検索',
+
+    'SUB_HEADING_TITLE' => '注文情報',
+
+    'HEADING_ORDER_NUMBER' => '注文番号%s',
+    'HEADING_ORDER_DATE' => '注文日：',
+    'HEADING_ORDER_TOTAL' => '注文合計：',
+
+    'HEADING_PRODUCTS' => '商品',
+    'HEADING_TAX' => '消費税',
+    'HEADING_TOTAL' => '合計金額',
+    'HEADING_QUANTITY' => '数量',
+
+    'HEADING_SHIPPING_METHOD' => '配送方法',
+    'HEADING_PAYMENT_METHOD' => '支払方法',
+
+    'HEADING_ORDER_HISTORY' => 'ステータス履歴とコメント',
+    'TEXT_NO_COMMENTS_AVAILABLE' => 'コメントはありません。',
+    'TABLE_HEADING_STATUS_DATE' => '日付',
+    'TABLE_HEADING_STATUS_ORDER_STATUS' => '注文状況',
+    'TABLE_HEADING_STATUS_COMMENTS' => 'コメント',
+    'QUANTITY_SUFFIX' => '&nbsp;',
+    'ORDER_HEADING_DIVIDER' => '&nbsp;-&nbsp;',
+    'TEXT_OPTION_DIVIDER' => '&nbsp;-&nbsp;',
+
+    'ENTRY_EMAIL' => '電子メールアドレス：',
+    'ENTRY_ORDER_NUMBER' => '注文番号：',
+
+    'ERROR_INVALID_EMAIL' => '無効な電子メールアドレスを入力しました。',
+    'ERROR_INVALID_ORDER' => '無効な注文番号を入力しました。',
+    'ERROR_NO_MATCH' => '入力内容に一致するものが見つかりません。',
+
+    'TEXT_LOOKUP_INSTRUCTIONS' => '注文のステータスを確認するには、注文番号と注文時のメールアドレスを入力してください。',
+
+    'FOOTER_DOWNLOAD' => '後で \'%s\' から商品をダウンロードすることも可能です。',
+    'FOOTER_DOWNLOAD_COWOA' => '最大ダウンロード数に達するか、時間がなくなるまで、「注文状況ページ」を使用して商品をダウンロードできます。',
+];
+return $define;
--- includes/languages/japanese/lang.order_status.php
+++
@@ -1,8 +0,0 @@
-<?php
-$define = [
-    'NAVBAR_TITLE' => '状況',
-    'NAVBAR_TITLE_1' => 'マイページ',
-    'HEADING_TITLE' => '注文履歴の確認',
-];
-
-return $define;
