e46bb7e41652ff9fa9b5635ba30f0a1c46c21353
piloujp <117885587+piloujp@users.noreply.github.com>
2024-09-04 18:53:31 +0900

    Update admin menu languages files for new language display method

---
+++ FILES_to_INSTALL/admin/includes/languages/japanese/extra_definitions/lang.posm.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: piloujp 2024 Aug 30 Modified in v2.1.0-alpha1 $
+*/
+
+$define = [
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_POSM' => 'このプラグインを使用すると、サイトで商品と属性の組み合わせ（「製品バリアント」または「属性別在庫」とも呼ばれます）に基づいて商品の在庫レベルとモデル番号を割り当てることができます。',
+];
+
+return $define;
