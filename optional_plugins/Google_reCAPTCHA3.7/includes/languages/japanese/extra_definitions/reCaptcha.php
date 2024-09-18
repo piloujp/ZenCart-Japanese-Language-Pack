11d6576dc2feca211fc5f63b29d66634b95e41cb
piloujp <117885587+piloujp@users.noreply.github.com>
2024-09-18 13:23:52 +0900

    Few plugins language files update
    New plugin name translation loading
    ReCaptcha 3.7 plugin translation added

---
+++ optional_plugins/Google_reCAPTCHA3.7/includes/languages/japanese/extra_definitions/reCaptcha.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package languageDefines
+ * @copyright Copyright 2003-2010 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: cardinal3dsecure.php 15757 2010-03-31 19:13:24Z drbyte $
+ */
+
+	define('RECAPTCHA_MISSING_INPUT_SECRET' , '入力シークレットがありません');
+	define('RECAPTCHA_INVALID_INPUT_SECRET' , '無効な入力シークレット');
+	define('RECAPTCHA_MISSING_INPUT_RESPONSE' , 'ロボットでないことを証明してください');
+	define('RECAPTCHA_INVALID_INPUT_RESPONSE' , '申し訳ありませんが、あなたがロボットではないことをもう一度証明してください');
