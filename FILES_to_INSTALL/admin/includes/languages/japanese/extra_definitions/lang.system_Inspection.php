fe7632c233a3f9120a633d53c2e30aff0f6377cc
piloujp <117885587+piloujp@users.noreply.github.com>
2024-09-26 17:22:58 +0900

    Mod plugin translation added
    Mod plugin translation added
    Removed useless or temporary files

---
+++ FILES_to_INSTALL/admin/includes/languages/japanese/extra_definitions/lang.system_Inspection.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: piloujp 2024 Sept 26 Modified in v2.1.0-alpha2 $
+*/
+
+$define = [
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_SYSTEM_INSPECTION' => '基本 Zen Cart インストールに対して行われたデータベースの変更を一覧表示します。（以前は ModList と呼ばれていました）',
+];
+
+return $define;
---
+++ FILES_to_INSTALL/zc_plugins/System Inspection/1.5.0/admin/includes/languages/japanese/lang.system_inspection.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * System Inspection (formerly Mod List by That Software Guy)
+ *
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
+ * @copyright Copyright 2015-2023 That Software Guy
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id:  $
+ */
+return [
+    'HEADING_TITLE' => 'Zen Cartのインストールの変更',
+    'PRESENT_ZENCART_VERSION_NUMBER' => 'このストアは現在 Zen Cart バージョン %s を使用しています',
+    'HEADING_WHY' => 'このレポートには、デフォルトの Zen Cart インストールとの違いを示す追加事項や異常事項が表示されます。',
+    'PAGES_TABLE' => '新しい管理者ページ',
+    'PLUGINS_LIST' => 'インストールされたプラグイン',
+    'DB_LIST' => '新しいデータベーステーブル',
+    'MODULE_LIST' => '新しいモジュール',
+    'MISSING_ADMIN_PAGES' => '管理者設定ページが見つからない',
+    'HEADING_PAGE_NAME' => 'ページ名',
+    'HEADING_PAGE_MENU_KEY' => 'メニューセクションキー',
+    'HEADING_PAGE_LINK' => 'リンク',
+    'HEADING_DISPLAY' => 'フラグを表示',
+    'MODULES_SUBNOTE' => '注意： 以下は、「有効」(ステータス = true) のモジュールのみを参照します。必ずしも、新しい「ファイルは存在するが使用されていない」モジュールを報告するわけではありません。',
+    'NO_EXTRAS' => '「新しい」モジュールが見つかりません。',
+    'NO_LINK' => '「リンクを作成できません」',
+    'NO_PAGES_TABLE_FOUND' => '問題: 管理ページが見つかりません。',
+    'NO_INFORMATION_SCHEMA_TABLE_FOUND' => 'INFORMATION_SCHEMA.TABLES テーブルが見つかりません ',
+    'MISSING_ADMIN_PAGES_WHY' => "これらは、対応する admin_pages エントリがない、Admin->Configuration (configuration_groups) 内のエントリです。",
+    'NO_NEW_PAGES' => '新しい管理ページはありません。',
+    'NO_NEW_TABLES' => '新しいテーブルはありません。',
+    'NO_PLUGINS_ENABLED' => '有効なプラグインが見つかりません。',
+    'NO_MISSING_ADMIN_PAGES' => '欠落している管理ページは見つかりませんでした。',
+    'MISSING_TABLE' => 'テーブルがありません： %s',
+    'NEW_MODULE_FOUND' => '新しいモジュール： %s',
+    'OBSOLETE_MODULE' => 'モジュール %s は廃止されました。削除することをお勧めします。',
+    'REPLACE_MODULE_WITH' => 'モジュール %1$s は %2$s に置き換える必要があります。',
+    'PLUGIN_FOUND' => '%1$s (%2$s)、バージョン %3$s (作成者: %4$s)',
+    'PAGES_PLUGIN_FOOTNOTE' => "注意： 上記の「新しい」ページの一部は、以下にリストされているプラ​​グインによって制御される場合があります。",
+];
