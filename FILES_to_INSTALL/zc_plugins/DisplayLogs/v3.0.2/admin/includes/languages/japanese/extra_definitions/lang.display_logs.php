5d190d433b3bbdc496475470e72da13048bdc600
piloujp <117885587+piloujp@users.noreply.github.com>
2024-10-23 05:45:30 +0900

    Display log forgotten strings translation

--- FILES_to_INSTALL/zc_plugins/DisplayLogs/v3.0.2/admin/includes/languages/japanese/extra_definitions/lang.display_logs.php
+++ FILES_to_INSTALL/zc_plugins/DisplayLogs/v3.0.2/admin/includes/languages/japanese/extra_definitions/lang.display_logs.php
@@ -9,6 +9,15 @@
 $define = [
     'ADMIN_PLUGIN_MANAGER_NAME_FOR_DISPLAYLOGS' => 'ログ表示',
     'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_DISPLAYLOGS' => 'Zen Cart ログ ファイルを表示および管理します。',
+// Display Logs - configuration
+    'CFGTITLE_DISPLAY_LOGS_MAX_DISPLAY' => 'ログ表示： 最大表示',
+    'CFGDESC_DISPLAY_LOGS_MAX_DISPLAY' => '表示するログの最大数を指定します。（デフォルト： <b>20</b>）',
+    'CFGTITLE_DISPLAY_LOGS_MAX_FILE_SIZE' => 'ログ表示： 最大ファイルサイズ',
+    'CFGDESC_DISPLAY_LOGS_MAX_FILE_SIZE' => '表示するファイルの最大サイズを指定します。（デフォルト： <b>80000</b>）',
+    'CFGTITLE_DISPLAY_LOGS_INCLUDED_FILES' => 'ログ表示： 含まれるファイルのプレフィックス',
+    'CFGDESC_DISPLAY_LOGS_INCLUDED_FILES' => '表示に含めるログ ファイルの <em>プレフィックス</em> をパイプ文字（|）で区切って指定します。間に挟まれたスペースは処理コードによって削除されます。',
+    'CFGTITLE_DISPLAY_LOGS_EXCLUDED_FILES' => 'ログ表示： 除外されるファイルプレフィックス',
+    'CFGDESC_DISPLAY_LOGS_EXCLUDED_FILES' => '表示から除外するログ ファイルのプレフィックスをパイプ文字 (|) で区切って指定します。間に挟まれたスペースは処理コードによって削除されます。',
 ];
 
 return $define;
