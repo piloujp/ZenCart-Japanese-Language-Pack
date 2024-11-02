7588def5c65cc685f7405943ec678d3ed86e4e51
piloujp <117885587+piloujp@users.noreply.github.com>
2024-11-02 17:42:10 +0900

    Plugins files re-organisation and doc update

---
+++ optional_plugins/Encapsulated/PostAuto/v2.0.0/admin/includes/languages/japanese/extra_definitions/lang.postauto.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: piloujp 2024 Oct 26 Modified in v2.1.0-beta1 $
+*/
+
+$define = [
+    'ADMIN_PLUGIN_MANAGER_NAME_FOR_POSTAUTO' => '郵便番号で自動入力',
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_POSTAUTO' => 'このプラグインは、郵便番号が入力されると、フォームのフィールドに都道府県、市区町村、および（データが利用可能な場合）番地を自動入力します。',
+];
+
+return $define;
