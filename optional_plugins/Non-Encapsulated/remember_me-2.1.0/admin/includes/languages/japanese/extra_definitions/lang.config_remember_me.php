43c3749cedebb77e5edcca79b2ef07a73e44c418
piloujp <117885587+piloujp@users.noreply.github.com>
2024-11-17 15:17:15 +0900

    Added Printable Price List plugin translation

---
+++ optional_plugins/Non-Encapsulated/remember_me-2.1.0/admin/includes/languages/japanese/extra_definitions/lang.config_remember_me.php
@@ -0,0 +1,14 @@
+<?php
+
+$define = [
+    'CFGTITLE_PERMANENT_LOGIN' => '自動ログインを有効にしますか？',
+    'CFGDESC_PERMANENT_LOGIN' => '自動ログイン機能を有効にすると、リピーターの顧客が自動的にログインします。',
+    'CFGTITLE_PERMANENT_LOGIN_EXPIRES' => '自動ログイン（クッキーの有効期間）',
+    'CFGDESC_PERMANENT_LOGIN_EXPIRES' => '顧客のログイン状態を維持する日数を設定します。',
+    'CFGTITLE_PERMANENT_LOGIN_SECRET' => '自動ログイン（シークレットキー）',
+    'CFGDESC_PERMANENT_LOGIN_SECRET' => '「ログイン情報を記憶します」クッキーを保存するときに秘密キーとして使用されるランダムな文字をいくつか選択します。',
+    'CFGTITLE_PERMANENT_LOGIN_CHECKBOX_DEFAULT' => '自動ログイン（チェックボックスのデフォルト）',
+    'CFGDESC_PERMANENT_LOGIN_CHECKBOX_DEFAULT' => '<code>ログイン</code> ページと <code>アカウント作成</code> ページにある <b>ログイン情報を記憶しますか？</b> チェックボックスの表示のデフォルト値を特定します。',
+];
+
+return $define;
---
+++ optional_plugins/Non-Encapsulated/remember_me-2.1.0/includes/languages/japanese/extra_definitions/remember_me_definitions.php
@@ -0,0 +1,9 @@
+<?php
+// -----
+// Part of the "Remember Me" plugin, modified for operation under Zen Cart v1.5.0 and later
+// by Cindy Merkin (aka lat9) of Vinos de Frutas Tropicales (vinosdefrutastropicales.com).
+//
+// Copyright (C) 2014-2020.  Vinos de Frutas Tropicales
+//
+define('TEXT_REMEMBER_ME', 'ログイン情報を記憶しますか？');
+define('TEXT_REMEMBER_ME_ALT', '次回のアクセス時に自動的にログインするには、このボックスにチェックを入れてください。 注意： これにより、この Web サイトのみが読み取ることができる Cookie が現在のブラウザに作成されます。');
