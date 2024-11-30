b863e9ac5404a984ceff85b6f2d7d55a8f64ae09
piloujp <117885587+piloujp@users.noreply.github.com>
2024-11-30 20:44:21 +0900

    Minor update sql files

--- FILES_to_INSTALL/zc_install/sql/updates/mysql_upgrade_zencart_210210.sql
+++ FILES_to_INSTALL/zc_install/sql/updates/mysql_upgrade_zencart_210210.sql
@@ -112,9 +112,9 @@ INSERT INTO orders_status VALUES ('5', '1', 'Sent', 15);
 
 #PROGRESS_FEEDBACK:!TEXT=Updating address related data...
 #住所フォーマット
-SELECT IFNULL((SELECT address_format_id FROM address_format WHERE address_format LIKE '〒%'), NULL) INTO @Formid;
+SET @Formid = (SELECT address_format_id FROM address_format WHERE address_format LIKE '〒%' ORDER BY address_format_id DESC LIMIT 1);
 REPLACE INTO address_format (address_format_id, address_format, address_summary) VALUES (@formid, '〒$postcode$cr$state$city$streets$cr$lastname $firstname 様', '$city $country');
-UPDATE countries SET address_format_id = (SELECT address_format_id from address_format WHERE address_format LIKE '〒%') WHERE countries_id = @japan_id;
+UPDATE countries SET address_format_id = (SELECT address_format_id FROM address_format WHERE address_format LIKE '〒%' ORDER BY address_format_id DESC LIMIT 1) WHERE countries_id = @japan_id;
 
 #PROGRESS_FEEDBACK:!TEXT=Updating admin configuration...
 #単位を kg と cm に設定します
--- sql/version_200_to_210_update.sql
+++ sql/version_200_to_210_update.sql
@@ -1,9 +1,9 @@
-Set @japan_id = (Select countries_id from countries where countries_iso_code_2 = 'JP' LIMIT 1);
+SET @japan_id = (SELECT countries_id FROM countries WHERE countries_iso_code_2 = 'JP' LIMIT 1);
 
 #住所フォーマット
-SELECT (SELECT address_format_id FROM address_format WHERE address_format LIKE '〒%') INTO @Formid;
+SET @Formid = (SELECT address_format_id FROM address_format WHERE address_format LIKE '〒%' ORDER BY address_format_id DESC LIMIT 1);
 REPLACE INTO address_format (address_format_id, address_format, address_summary) VALUES (@formid, '〒$postcode$cr$state$city$streets$cr$lastname $firstname 様', '$city $country');
-UPDATE countries SET address_format_id = @Formid WHERE countries_id = @japan_id;
+UPDATE countries SET address_format_id = (SELECT address_format_id FROM address_format WHERE address_format LIKE '〒%' ORDER BY address_format_id DESC LIMIT 1) WHERE countries_id = @japan_id;
 
 #NEXT_X_ROWS_AS_ONE_COMMAND:3
 INSERT INTO project_version_history (project_version_key, project_version_major, project_version_minor, project_version_patch, project_version_date_applied, project_version_comment)
