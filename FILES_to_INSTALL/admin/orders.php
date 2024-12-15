46d661b928a04c7ce824a88dcbcc4a1d764c4938
piloujp <117885587+piloujp@users.noreply.github.com>
2024-12-16 00:40:13 +0900

    PHP 8.4 warning/error when string is Null correction

--- FILES_to_INSTALL/admin/orders.php
+++ FILES_to_INSTALL/admin/orders.php
@@ -587,7 +587,7 @@ if (!empty($action) && $order_exists === true) {
                     <button type="button" class="btn btn-xs btn-default mt-3" title="<?= TEXT_COPY ?>" onclick="copyToClipboard('customer', this)"><?= TEXT_COPY ?></button>
                 </td>
                 <td><?php echo zen_address_format($order->customer['format_id'], $order->customer, 1, '', '<br>');
-					echo !empty(trim($order->customer['name_kana'])) ? '<br><small>' . ENTRY_NAME_KANA . $order->customer['name_kana'] . '</small>' : '';
+					echo !empty($order->customer['name_kana']) ? '<br><small>' . ENTRY_NAME_KANA . $order->customer['name_kana'] . '</small>' : '';
 				?></td>
               </tr>
               <tr>
@@ -665,9 +665,9 @@ if (!empty($action) && $order_exists === true) {
                     <?php } ?>
                 </td>
                 <td><?php echo (empty($order->delivery)) ? TEXT_NONE : zen_address_format($order->delivery['format_id'], $order->delivery, 1, '', '<br>');
-					echo !empty(trim($order->delivery['name_kana'])) ? '<br><small>' . ENTRY_NAME_KANA . $order->delivery['name_kana'] . '</small>' : '';
-					echo !empty(trim($order->delivery['telephone'])) ? '<br><small>' . ENTRY_TELEPHONE_NUMBER . $order->delivery['telephone'] . '</small>' : '';
-					echo !empty(trim($order->delivery['timespec'])) ? '<br><small>' . TEXT_TIME_SPECIFY . $order->delivery['timespec'] . '</small>' :'';
+					echo !empty($order->delivery['name_kana']) ? '<br><small>' . ENTRY_NAME_KANA . $order->delivery['name_kana'] . '</small>' : '';
+					echo !empty($order->delivery['telephone']) ? '<br><small>' . ENTRY_TELEPHONE_NUMBER . $order->delivery['telephone'] . '</small>' : '';
+					echo !empty($order->delivery['timespec']) ? '<br><small>' . TEXT_TIME_SPECIFY . $order->delivery['timespec'] . '</small>' :'';
 				?></td>
               </tr>
 <?php if (!empty($order->delivery)) { ?>
@@ -704,8 +704,8 @@ if (!empty($action) && $order_exists === true) {
                     <button type="button" class="btn btn-xs btn-default mt-3" title="<?= TEXT_COPY ?>" onclick="copyToClipboard('billing', this)"><?= TEXT_COPY ?></button>
                 </td>
                 <td><?php echo zen_address_format($order->billing['format_id'], $order->billing, 1, '', '<br>');
-					echo !empty(trim($order->billing['name_kana'])) ? '<br><small>' . ENTRY_NAME_KANA . $order->billing['name_kana'] . '</small>' : '';
-					echo !empty(trim($order->billing['telephone'])) ? '<br><small>' . ENTRY_TELEPHONE_NUMBER . $order->billing['telephone'] . '</small>' : '';
+					echo !empty($order->billing['name_kana']) ? '<br><small>' . ENTRY_NAME_KANA . $order->billing['name_kana'] . '</small>' : '';
+					echo !empty($order->billing['telephone']) ? '<br><small>' . ENTRY_TELEPHONE_NUMBER . $order->billing['telephone'] . '</small>' : '';
 				?></td>
               </tr>
               <tr>
