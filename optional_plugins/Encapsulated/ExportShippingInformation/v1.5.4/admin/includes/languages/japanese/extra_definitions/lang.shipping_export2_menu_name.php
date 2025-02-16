18c03dd5fdc98ebe1d554067b703e3849f85a362
piloujp <117885587+piloujp@users.noreply.github.com>
2025-02-16 17:31:31 +0900

    Added Export Shipping Information plugin translation

---
+++ optional_plugins/Encapsulated/ExportShippingInformation/v1.5.4/admin/includes/languages/japanese/extra_definitions/lang.shipping_export2_menu_name.php
@@ -0,0 +1,19 @@
+<?php
+/*
+//////////////////////////////////////////////////////////
+//  SHIPPING EXPORT                                        //
+//                                                      //
+//                                                      //
+//  Released under the GNU General Public License       //
+//  available at www.zen-cart.com/license/2_0.txt       //
+//  or see "license.txt" in the downloaded zip          //
+//////////////////////////////////////////////////////////
+//  DESCRIPTION: The name of the export shipping report file.     //
+//  You should not ever have to edit this file.         //
+//////////////////////////////////////////////////////////
+*/
+// Last updated: Shippping Export II, v1.5.3
+$define = [
+    'BOX_TOOLS_SHIPPING_EXPORT2' => '輸出注文配送情報 II',
+];
+return $define;
