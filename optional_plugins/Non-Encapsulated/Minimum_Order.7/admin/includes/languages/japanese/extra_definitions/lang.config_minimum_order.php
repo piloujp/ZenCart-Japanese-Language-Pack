906490290be66b2165a69f04cb0d53f5f2aef266
piloujp <117885587+piloujp@users.noreply.github.com>
2025-02-22 16:56:03 +0900

    Added Minimum Order plugin admin menus translation

---
+++ optional_plugins/Non-Encapsulated/Minimum_Order.7/admin/includes/languages/japanese/extra_definitions/lang.config_minimum_order.php
@@ -0,0 +1,10 @@
+<?php
+
+$define = [
+    'CFGTITLE_MIN_ORDER_AMOUNT' => '最低注文金額',
+    'CFGDESC_MIN_ORDER_AMOUNT' => 'チェックアウトするために注文の合計に必要な最低金額。最小値がないように空白のままにしておきます。',
+    'CFGTITLE_MIN_FIRST_ORDER_AMOUNT' => '初回注文の最低金額',
+    'CFGDESC_MIN_FIRST_ORDER_AMOUNT' => 'チェックアウトするために最初の注文の合計に必要な最低金額。最小値がないように空白のままにしておきます。常に同じ最低金額が必要な場合は、最低注文金額と同じにしてください。',
+];
+
+return $define;
