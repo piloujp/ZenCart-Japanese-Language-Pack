2d7a54f920688ad907c94574bb195c5543df7973
piloujp <117885587+piloujp@users.noreply.github.com>
2025-02-25 03:42:39 +0900

    Added AbuseIPDB plugin translation

---
+++ optional_plugins/Encapsulated/AbuseIPDB/v3.0.4/admin/includes/languages/japanese/extra_definitions/lang.abuseipdb_admin_names.php
@@ -0,0 +1,16 @@
+<?php
+ /**
+ * Module: AbuseIPDB
+ *
+ * @requires    Zen Cart 2.1.0 or later, PHP 7.4+ (recommended: PHP 8.x)
+ * @author      Marcopolo
+ * @copyright   2023-2025
+ * @license     GNU General Public License (GPL) - https://www.gnu.org/licenses/gpl-3.0.html
+ * @version     3.0.1
+ * @updated     1-22-2025
+ * @github      https://github.com/CcMarc/AbuseIPDB
+ */
+return [
+    'BOX_ABUSEIPDB_NAME' => 'AbuseIPDB設定',
+    'BOX_ABUSEIPDB_HEADER' => 'AbuseIPDB 寄稿者'
+];
