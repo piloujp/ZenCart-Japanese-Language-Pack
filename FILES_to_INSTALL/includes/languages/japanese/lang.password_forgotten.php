fb1d66301a8e7361517834f3b197f048ae35cf83
piloujp <117885587+piloujp@users.noreply.github.com>
2025-02-28 13:34:38 +0900

    Update lang.password_forgotten.php

--- FILES_to_INSTALL/includes/languages/japanese/lang.password_forgotten.php
+++ FILES_to_INSTALL/includes/languages/japanese/lang.password_forgotten.php
@@ -4,7 +4,7 @@ $define = [
     'NAVBAR_TITLE_2' => 'パスワードを忘れたら',
     'HEADING_TITLE' => 'パスワードをお忘れですか?',
     'TEXT_MAIN' => '以下のメールアドレスを入力すると、パスワードをリセットする方法についての手順が送信されます。',
-    'EMAIL_PASSWORD_RESET_SUBJECT' => '%%STORE_NAME%% - パスワードのリセット',
+    'EMAIL_PASSWORD_RESET_SUBJECT' => STORE_NAME . ' - パスワードのリセット',
     'EMAIL_PASSWORD_RESET_BODY' => "%1s からパスワードのリセットが要求されました。\n\n'%2s' のパスワードをリセットするには、次の URL にアクセスしてください：\n\n%3s \n\n",
     'SUCCESS_PASSWORD_RESET_SENT' => '新しいパスワードをご登録のメールアドレス宛に送信しました。',
 ];
