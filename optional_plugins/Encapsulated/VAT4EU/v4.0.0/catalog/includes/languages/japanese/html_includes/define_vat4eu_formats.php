552cba1f40cb62dadf4fdadad00f6b34f0a35fc2
piloujp <117885587+piloujp@users.noreply.github.com>
2025-03-15 19:37:41 +0900

    Added encapsulated plugin VAT4EU v4.0.0 translation

---
+++ optional_plugins/Encapsulated/VAT4EU/v4.0.0/catalog/includes/languages/japanese/html_includes/define_vat4eu_formats.php
@@ -0,0 +1,158 @@
+<p>以下の表の「構造」列は、さまざまな加盟国に必要な形式を示しています。<var>0-9</var> を使用して数字を表し、<var>X</var> を使用して数字または文字を表します。</p>
+<p><b>注記：</b>長さには２文字の ISO コードは含まれません。</p>
+
+<table id="vat4eu-table" class="table table-striped">
+    <thead>
+        <tr>
+            <th>加盟国</th>
+            <th>構造</th>
+            <th>長さ</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>オーストリア - AT</td>
+            <td>AT<b>U</b>12345678</td>
+            <td>９文字<sup>1</sup></td>
+        </tr>
+        <tr>
+            <td>ベルギー - BE</td>
+            <td>BE1234567890</td>
+            <td>１０桁<sup>2</sup></td>
+        </tr>
+        <tr>
+            <td>ブルガリア - BG</td>
+            <td>BG123456789<br>BG1234567890</td>
+            <td>９または１０桁</td>
+        </tr>
+        <tr>
+            <td>クロアチア - HR</td>
+            <td>HR12345678901</td>
+            <td>１１桁</td>
+        </tr>
+        <tr>
+            <td>キプロス - CY</td>
+            <td>CY12345678L</td>
+            <td>９文字</td>
+        </tr>
+        <tr>
+            <td>チェコ共和国 - CZ</td>
+            <td>CZ12345678<br>CZ123456789<br>CZ1234567890</td>
+            <td>８、９または１０桁</td>
+        </tr>
+        <tr>
+            <td>デンマーク - DK</td>
+            <td>DK12345678</td>
+            <td>８桁</td>
+        </tr>
+        <tr>
+            <td>エストニア - EE</td>
+            <td>EE123456789</td>
+            <td>９桁</td>
+        </tr>
+        <tr>
+            <td>フィンランド - FI</td>
+            <td>FI12345678</td>
+            <td>８桁</td>
+        </tr>
+        <tr>
+            <td>フランス - FR</td>
+            <td>FR12345678901<br>FRX1234567890<br>FR1X234567890<br>FRXX123456789</td>
+            <td>１１文字<sup>3</sup></td>
+        </tr>
+        <tr>
+            <td>ドイツ - DE</td>
+            <td>DE999999999</td>
+            <td>９桁</td>
+        </tr>
+        <tr>
+            <td>ギリシャ - EL</td>
+            <td>EL123456789</td>
+            <td>９桁</td>
+        </tr>
+        <tr>
+            <td>ハンガリー - HU</td>
+            <td>HU12345678</td>
+            <td>８桁</td>
+        </tr>
+        <tr>
+            <td>アイルランド - IE</td>
+            <td>IE1234567X<br>IE1X23456X<br>IE1234567XX</td>
+            <td>８または９文字<sup>4</sup></td>
+        </tr>
+        <tr>
+            <td>イタリア - IT</td>
+            <td>IT12345678901</td>
+            <td>１１桁</td>
+        </tr>
+        <tr>
+            <td>ラトビア - LV</td>
+            <td>LV12345678901</td>
+            <td>１１桁</td>
+        </tr>
+        <tr>
+            <td>リトアニア - LT</td>
+            <td>LT123456789<br>LT123456789012</td>
+            <td>９ or １２桁</td>
+        </tr>
+        <tr>
+            <td>ルクセンブルク - LU</td>
+            <td>LU12345678</td>
+            <td>８桁</td>
+        </tr>
+        <tr>
+            <td>マルタ - MT</td>
+            <td>MT12345678</td>
+            <td>８桁</td>
+        </tr>
+        <tr>
+            <td>オランダ - NL</td>
+            <td>NL123456789<b>B</b>01</td>
+            <td>１２文字<sup>5</sup></td>
+        </tr>
+        <tr>
+            <td>ポーランド - PL</td>
+            <td>PL1234567890</td>
+            <td>１０桁</td>
+        </tr>
+        <tr>
+            <td>ポルトガル - PT</td>
+            <td>PT123456789</td>
+            <td>９桁</td>
+        </tr>
+        <tr>
+            <td>ルーマニア - RO</td>
+            <td>RO12<br>RO123<br>RO1234<br>RO12345<br>RO123456<br>RO123456<br>RO1234567<br>RO12345678<br>RO123456789<br>RO1234567890</td>
+            <td>２桁から１０桁まで</td>
+        </tr>
+        <tr>
+            <td>Slovakia - SK</td>
+            <td>SK1234567890</td>
+            <td>１０桁</td>
+        </tr>
+        <tr>
+            <td>スロベニア - SI</td>
+            <td>SI12345678</td>
+            <td>８桁</td>
+        </tr>
+        <tr>
+            <td>スペイン - ES</td>
+            <td>ESX12345678<br>ES12345678X<br>ESX1234567X</td>
+            <td>９文字<sup>6</sup></td>
+        </tr>
+        <tr>
+            <td>スウェーデン - SE</td>
+            <td>SE123456789012</td>
+            <td>１２桁</td>
+        </tr>
+    </tbody>
+</table>
+<p class="py-2"><strong>Notes:</strong></p>
+<ul id="vat4eu-notes" class="list-unstyled">
+    <li><sup>1</sup> プレフィックスに続く最初の位置は常に <b>U</b> です。</li>
+    <li><sup>2</sup> 顧客が９桁の VAT 番号を提供する場合は、最初の桁として <b>0</b> を追加する必要があります。</li>
+    <li><sup>3</sup> 文字は最初の文字および/または２番目の文字として含めることができます（文字 O と I を除く）。</li>
+    <li><sup>4</sup> 文字は、最後、２番目と最後、または最後の２文字として含めることができます。</li>
+    <li><sup>5</sup> プレフィックスに続く１０番目の位置は常に <b>B</b> です。</li>
+    <li><sup>6</sup> 最初と最後の文字は文字にすることができます。</li>
+</ul>
