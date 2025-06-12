697d57d32a10a9f5012dfc10d708fd2bfcb91bb0
piloujp <117885587+piloujp@users.noreply.github.com>
2025-06-12 15:38:20 +0900

    Added offsite payment translation

---
+++ optional_plugins/Non-Encapsulated/offsitepayment-v3/includes/languages/japanese/modules/payment/lang.offsitepayment.php
@@ -0,0 +1,18 @@
+<?php
+
+$define = [
+    'MODULE_PAYMENT_OFFSITEPAYMENT_TEXT_TITLE' => '請求書発行 / 後払い',
+    'MODULE_PAYMENT_OFFSITEPAYMENT_TEXT_DESCRIPTION' => '',
+    'MODULE_PAYMENT_OFFSITEPAYMENT_TEXT_EMAIL_FOOTER' => '', // '注文合計金額のお支払い依頼をメールでお送りします。'
+// Beginning of constant configuration titles and descriptions for payment module offsitepayment
+    'CFGTITLE_MODULE_PAYMENT_OFFSITEPAYMENT_STATUS' => 'オフサイト支払いモジュールを有効にする',
+    'CFGDESC_MODULE_PAYMENT_OFFSITEPAYMENT_STATUS' => 'オフサイトの支払いを受け入れますか？',
+    'CFGTITLE_MODULE_PAYMENT_OFFSITEPAYMENT_SORT_ORDER' => '表示の並べ替え順序。',
+    'CFGDESC_MODULE_PAYMENT_OFFSITEPAYMENT_SORT_ORDER' => '表示の並び順。最下位から順に表示します。',
+    'CFGTITLE_MODULE_PAYMENT_OFFSITEPAYMENT_ZONE' => '支払い地域',
+    'CFGDESC_MODULE_PAYMENT_OFFSITEPAYMENT_ZONE' => 'ゾーンを選択した場合は、そのゾーンに対してのみこの支払い方法が有効になります。',
+    'CFGTITLE_MODULE_PAYMENT_OFFSITEPAYMENT_ORDER_STATUS_ID' => '注文ステータスの設定',
+    'CFGDESC_MODULE_PAYMENT_OFFSITEPAYMENT_ORDER_STATUS_ID' => 'この支払いモジュールで行われた注文のステータスをこの値に設定します',
+];
+
+return $define;
