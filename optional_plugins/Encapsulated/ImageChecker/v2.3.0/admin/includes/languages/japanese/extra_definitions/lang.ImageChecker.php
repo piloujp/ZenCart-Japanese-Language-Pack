9c8f9bf6bc10c231b15c51ac17f1b5946cf2b658
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-27 04:54:25 +0900

    Plugins files names update to use their unique key

--- optional_plugins/Encapsulated/ImageChecker/v2.3.0/admin/includes/languages/japanese/extra_definitions/lang.ImageChecker.php
+++ optional_plugins/Encapsulated/ImageChecker/v2.3.0/admin/includes/languages/japanese/extra_definitions/lang.ImageChecker.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * @copyright Copyright 2003-2025 Zen Cart Development Team
- * @copyright Portions Copyright 2003 osCommerce
- * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: piloujp 2025 Aug 8 Modified in v2.1.0 $
-*/
-
 $define = [
     'ADMIN_PLUGIN_MANAGER_NAME_FOR_IMAGECHECKER' => '画像チェッカー',
     'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_IMAGECHECKER' => '不足している画像や無効な画像がないか確認します。',
---
+++ optional_plugins/Encapsulated/MultipleProductCopy/v2.0.0/admin/includes/languages/japanese/extra_definitions/lang.MultipleProductCopy.php
@@ -0,0 +1,7 @@
+<?php
+$define = [
+    'ADMIN_PLUGIN_MANAGER_NAME_FOR_MULTIPLEPRODUCTCOPY' => '複数の商品のコピー',
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_MULTIPLEPRODUCTCOPY' => 'リンクのコピー、複製、移動、削除などの一括商品アクションを実行します。',
+];
+
+return $define;
