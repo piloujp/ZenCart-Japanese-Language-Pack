6d51a7a6d4b5315557334a97dcec7f86f32902ca
piloujp <117885587+piloujp@users.noreply.github.com>
2025-09-01 05:52:22 +0900

    Plugins files renaming

--- optional_plugins/Encapsulated/ExportShippingInformation/v1.5.6/admin/includes/languages/japanese/extra_definitions/lang.shipping_export2_menu_name.php
+++ optional_plugins/Encapsulated/ExportShippingInformation/v1.5.6/admin/includes/languages/japanese/extra_definitions/lang.shipping_export2_menu_name.php
@@ -12,7 +12,7 @@
 //  You should not ever have to edit this file.         //
 //////////////////////////////////////////////////////////
 */
-// Last updated: Shippping Export II, v1.5.3
+// Last updated: Shippping Export II, v1.5.4
 $define = [
     'BOX_TOOLS_SHIPPING_EXPORT2' => '輸出注文配送情報 II',
 ];
