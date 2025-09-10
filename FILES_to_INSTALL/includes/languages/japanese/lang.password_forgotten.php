ca6b31fbcbd9113969cbcd41f8b1117b9a2a97bb
piloujp <117885587+piloujp@users.noreply.github.com>
2025-09-10 11:05:21 +0900

    Update to last ZC Files

--- FILES_to_INSTALL/includes/languages/japanese/lang.password_forgotten.php
+++ FILES_to_INSTALL/includes/languages/japanese/lang.password_forgotten.php
@@ -5,7 +5,7 @@ $define = [
     'HEADING_TITLE' => 'パスワードをお忘れですか?',
     'TEXT_MAIN' => '以下のメールアドレスを入力すると、パスワードをリセットする方法についての手順が送信されます。',
     'EMAIL_PASSWORD_RESET_SUBJECT' => STORE_NAME . ' - パスワードのリセット',
-    'EMAIL_PASSWORD_RESET_BODY' => "%1s からパスワードのリセットが要求されました。\n\n'%2s' のパスワードをリセットするには、次の URL にアクセスしてください：\n\n%3s \n\n",
+    'EMAIL_PASSWORD_RESET_BODY' => "%1\$s からパスワードのリセットが要求されました。\n\n'%2\$s' のパスワードをリセットするには、次の URL にアクセスしてください：\n\n%3\$s\n\n",
     'SUCCESS_PASSWORD_RESET_SENT' => '新しいパスワードをご登録のメールアドレス宛に送信しました。',
 ];
 
