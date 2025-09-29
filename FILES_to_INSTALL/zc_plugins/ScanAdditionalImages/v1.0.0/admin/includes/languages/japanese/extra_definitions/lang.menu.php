58c09997e8de10244fa91bd70dfe9fd773c35d3b
piloujp <117885587+piloujp@users.noreply.github.com>
2025-09-29 23:12:03 +0900

    Languages files update for Scan Additional Images feature

--- FILES_to_INSTALL/zc_plugins/ScanAdditionalImages/v1.0.0/admin/includes/languages/japanese/extra_definitions/lang.menu.php
+++ FILES_to_INSTALL/zc_plugins/ScanAdditionalImages/v1.0.0/admin/includes/languages/japanese/extra_definitions/lang.menu.php
@@ -2,6 +2,8 @@
 $define = [
     'ADMIN_PLUGIN_MANAGER_NAME_FOR_SCANADDITIONALIMAGES' => 'データベース用の追加商品画像ファイルをスキャン',
     'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_SCANADDITIONALIMAGES' => 'サーバー上で見つかった追加画像と一致する従来のファイル名パターンを製品にリンクするためのツール。',
+    'CFGTITLE_ADDITIONAL_IMAGES_MODE' => '追加画像ファイル名の一致パターン',
+    'CFGDESC_ADDITIONAL_IMAGES_MODE' => '「ファイル名マッチングモード」では、次の二つの形式で「_」サフィックスを使用できます：<br>「strict」= 常に「_」サフィックスを使用する<br>「legacy」= サブディレクトリでのみ「_」サフィックスを使用する<br>（v210 より前は legacy がデフォルトでした）<br>デフォルト = strict',
 ];
 
 return $define;
