7501f73ee615d7c150d81d1fc3dd5c16c5e4add1
piloujp <117885587+piloujp@users.noreply.github.com>
2025-10-04 03:30:47 +0900

    Scan Additional Images plugin language files update

--- FILES_to_INSTALL/zc_plugins/ScanAdditionalImages/v1.0.0/admin/includes/languages/japanese/extra_definitions/lang.scan_for_additional_images.php
+++ FILES_to_INSTALL/zc_plugins/ScanAdditionalImages/v1.0.0/admin/includes/languages/japanese/extra_definitions/lang.scan_for_additional_images.php
@@ -2,6 +2,9 @@
 
 $define = [
     'BOX_TOOLS_SCAN_FOR_IMAGES' => '追加の商品画像をデータベースにスキャンする',
+
+    'SYNC_TEXT_NOTHING_TO_PROCESS' => 'これらのバッチ選択で処理する商品（画像付き）はありません。「開始」と「バッチ サイズ」の設定を確認します。',
+    'SYNC_TEXT_QUERY_ONLY' => 'クエリのみ - 処理は実行されません。',
 ];
 
 return $define;
--- FILES_to_INSTALL/zc_plugins/ScanAdditionalImages/v1.0.0/catalog/includes/languages/japanese/extra_definitions/lang.scan_for_additional_images.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$define = [
-    'SYNC_TEXT_NOTHING_TO_PROCESS' => 'これらのバッチ選択で処理する商品（画像付き）はありません。',
-    'SYNC_TEXT_QUERY_ONLY' => 'クエリのみ - 処理は実行されません。',
-];
-
-return $define;
