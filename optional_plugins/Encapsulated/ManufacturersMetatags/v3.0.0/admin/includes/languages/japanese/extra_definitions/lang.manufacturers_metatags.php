60f9747ca0ac24a5208fc2120ad7523e688c66ad
piloujp <117885587+piloujp@users.noreply.github.com>
2025-10-30 10:54:33 +0900

    Added Manufacturers Metatags plugin's translation

---
+++ optional_plugins/Encapsulated/ManufacturersMetatags/v3.0.0/admin/includes/languages/japanese/extra_definitions/lang.manufacturers_metatags.php
@@ -0,0 +1,9 @@
+<?php
+// -----
+// Part of the Manufacturers Meta Tags plugin for Zen Cart.
+//
+return [
+    'TEXT_EDIT_MANUFACTURER_META_TAGS_TITLE' => 'メタタグタイトル：',
+    'TEXT_EDIT_MANUFACTURER_META_TAGS_KEYWORDS' => 'メタタグキーワード：',
+    'TEXT_EDIT_MANUFACTURERS_META_TAGS_DESCRIPTION' => 'メタタグの説明：',
+];
---
+++ optional_plugins/Encapsulated/ManufacturersMetatags/v3.0.0/admin/includes/languages/japanese/extra_definitions/lang.menu.php
@@ -0,0 +1,8 @@
+<?php
+
+$define = [
+    'ADMIN_PLUGIN_MANAGER_NAME_FOR_MANUFACTURERSMETATAGS' => 'メーカーのメタタグ',
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_MANUFACTURERSMETATAGS' => 'このプラグインを使用すると、Zen Cart に組み込まれている製品固有のメタタグ処理と同様の方法で、ストアでメーカー固有のメタタグのタイトル、キーワード、説明テキストを割り当てることができます。',
+];
+
+return $define;
