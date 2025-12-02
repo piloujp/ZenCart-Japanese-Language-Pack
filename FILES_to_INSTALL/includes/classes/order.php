bc5aa1f02b3e84be0fc20f9b3c70c2c5d8a491a5
piloujp <117885587+piloujp@users.noreply.github.com>
2025-12-02 15:37:21 +0900

    Update order.php

--- FILES_to_INSTALL/includes/classes/order.php
+++ FILES_to_INSTALL/includes/classes/order.php
@@ -25,7 +25,7 @@ class order extends base
      */
     public $attachArray = [];
     /**
-     * $bestSellersUpdate is a flag used in Notifier to prevent updating of the best sellers details.
+     * $bestSellersUpdate is a flag used in Notifier to prevent updating of the bestsellers details.
      * @var boolean
      */
     public $bestSellersUpdate;
@@ -105,7 +105,7 @@ class order extends base
      */
     public $queryReturnFlag;
     /**
-     * $send_low_stock_emails is a flag to indicate if a low stock email should be send. It may be modified by a notifier
+     * $send_low_stock_emails is a flag to indicate if a low stock email should be sent. It may be modified by a notifier
      * @var boolean
      */
     public $send_low_stock_emails;
@@ -548,6 +548,7 @@ class order extends base
             'tax_groups' => [],
             'comments' => (isset($_SESSION['comments']) ? $_SESSION['comments'] : ''),
             'ip_address' => $_SESSION['customers_ip_address'] . ' - ' . $_SERVER['REMOTE_ADDR'],
+            'language_code' => $_SESSION['languages_code'],
             'is_wholesale' => (int)Customer::isWholesaleCustomer(),
         ];
 
@@ -1380,7 +1381,7 @@ class order extends base
             zen_mail('', SEND_EXTRA_LOW_STOCK_EMAILS_TO, EMAIL_TEXT_SUBJECT_LOWSTOCK, $email_low_stock, STORE_OWNER, EMAIL_FROM, ['EMAIL_MESSAGE_HTML' => nl2br($email_low_stock)], 'low_stock');
         }
 
-        // lets start with the email confirmation
+        // let's start with the email confirmation
         // make an array to store the html version
         $html_msg = [];
         $japan_id = zen_country_iso_to_id('JP');
