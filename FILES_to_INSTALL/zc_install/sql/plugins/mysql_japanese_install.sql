6eb0b8d2df194d94d84d0c44ffd7f4bc152e733f
piloujp <117885587+piloujp@users.noreply.github.com>
2026-04-06 17:15:43 +0900

    Project Version update

--- FILES_to_INSTALL/zc_install/sql/plugins/mysql_japanese_install.sql
+++ FILES_to_INSTALL/zc_install/sql/plugins/mysql_japanese_install.sql
@@ -188,5 +188,5 @@ SELECT project_version_key, project_version_major, project_version_minor, projec
 FROM project_version;
 
 ## Now set to new version
-UPDATE project_version SET project_version_comment = 'New Installation-v220 with Japanese Pack v2.2.0', project_version_date_applied = now() WHERE project_version_key = 'Zen-Cart Main';
-UPDATE project_version SET project_version_minor = '2.0210', project_version_comment = 'New Installation-v220 with Japanese Pack v2.2.0', project_version_date_applied = now() WHERE project_version_key = 'Zen-Cart Database';
+UPDATE project_version SET project_version_comment = 'New Installation with Japanese Pack v2.2.1', project_version_date_applied = now() WHERE project_version_key = 'Zen-Cart Main';
+UPDATE project_version SET project_version_minor = '2.0210', project_version_comment = 'New Installation with Japanese Pack v2.2.1', project_version_date_applied = now() WHERE project_version_key = 'Zen-Cart Database';
