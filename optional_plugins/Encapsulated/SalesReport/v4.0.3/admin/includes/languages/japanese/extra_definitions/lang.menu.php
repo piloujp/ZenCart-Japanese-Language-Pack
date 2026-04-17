bc2f4c8de5feab2e978d4fc192349fc7ce4ed9e1
piloujp <117885587+piloujp@users.noreply.github.com>
2026-04-18 02:50:35 +0900

    Plugin Sales report II update to v4.0.3

---
+++ optional_plugins/Encapsulated/SalesReport/v4.0.3/admin/includes/languages/japanese/extra_definitions/lang.menu.php
@@ -0,0 +1,7 @@
+<?php
+$define = [
+    'ADMIN_PLUGIN_MANAGER_NAME_FOR_SALESREPORT' => '売上レポート II',
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_SALESREPORT' => 'サイトの売上に関するフィルタリングされたレポートを生成します。',
+];
+
+return $define;
