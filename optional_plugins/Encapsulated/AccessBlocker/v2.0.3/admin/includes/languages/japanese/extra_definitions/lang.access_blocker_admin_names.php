ff891cbab3285179736e92a3ba62d6dcb041ad50
piloujp <117885587+piloujp@users.noreply.github.com>
2026-04-26 04:24:33 +0900

    Plugins update and addition
    Access Blocker v2.0.3
    Ez Pages Meta-tags v5.0.0

---
+++ optional_plugins/Encapsulated/AccessBlocker/v2.0.3/admin/includes/languages/japanese/extra_definitions/lang.access_blocker_admin_names.php
@@ -0,0 +1,8 @@
+<?php
+// -----
+// Part of the Access Blocker plugin, created by lat9 (https://vinosdefrutastropicales.com)
+// Copyright (c) 2019-2025, Vinos de Frutas Tropicales.
+//
+return [
+    'BOX_ACCESSBLOCK_NAME' => 'アクセスブロッカー設定',
+];
---
+++ optional_plugins/Encapsulated/EzpagesMetatags/5.0.0/Installer/languages/japanese/main.php
@@ -0,0 +1,2 @@
+<?php
+zen_define_default('ERROR_UNABLE_TO_DELETE_FILE', '古いファイルの削除に失敗しました： %s');
