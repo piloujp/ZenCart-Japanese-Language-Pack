902abb1318a6784288c74ab6900bd56f0c76161d
piloujp <117885587+piloujp@users.noreply.github.com>
2026-04-29 02:44:48 +0900

    Revert "Address form update"
    This reverts commit 0977da1dba2bf48b0483e2750ae4e601c24ff9a2.

--- optional_templates/ZCA-Bootstrap-Template-3.7.9/includes/templates/bootstrap/templates/tpl_modules_common_address_format.php
+++ optional_templates/ZCA-Bootstrap-Template-3.7.9/includes/templates/bootstrap/templates/tpl_modules_common_address_format.php
@@ -103,7 +103,7 @@ if (ACCOUNT_COMPANY === 'true') {
 if (ACCOUNT_STATE === 'true') {
     if ($flag_show_pulldown_states === true) {
 ?>
-<label class="inputLabel" for="stateZone" id="zoneLabel"><?= ENTRY_STATE ?></label><?php if (!empty(ENTRY_STATE_TEXT) && $zone_id === 0) echo '<span class="alert">' . ENTRY_STATE_TEXT . '</span>' ?>
+<label class="inputLabel" for="stateZone" id="zoneLabel"><?= ENTRY_STATE ?></label><?php if (!empty(ENTRY_STATE_TEXT)) echo '<span class="alert">' . ENTRY_STATE_TEXT . '</span>' ?>
 
 <?php
       echo zen_draw_pull_down_menu('zone_id', zen_prepare_country_zones_pull_down($selected_country), $zone_id, 'id="stateZone"');
@@ -186,7 +186,7 @@ if (ACCOUNT_SUBURB === 'true') {
 if (ACCOUNT_STATE === 'true') {
     if ($flag_show_pulldown_states === true) {
 ?>
-<label class="inputLabel" for="stateZone" id="zoneLabel"><?= ENTRY_STATE ?></label><?php if (!empty(ENTRY_STATE_TEXT) && $zone_id === 0) echo '<span class="alert">' . ENTRY_STATE_TEXT . '</span>' ?>
+<label class="inputLabel" for="stateZone" id="zoneLabel"><?= ENTRY_STATE ?></label><?php if (!empty(ENTRY_STATE_TEXT)) echo '<span class="alert">' . ENTRY_STATE_TEXT . '</span>' ?>
 
 <?php
       echo zen_draw_pull_down_menu('zone_id', zen_prepare_country_zones_pull_down($selected_country), $zone_id, 'id="stateZone"');
