cd344c4f86c57b8378f493fcca1bfbcb0118cb45
piloujp <117885587+piloujp@users.noreply.github.com>
2026-05-24 05:45:01 +0900

    Update to lastest ZC files (zen_config)

--- FILES_to_INSTALL/includes/languages/japanese/modules/payment/lang.moneyorder.php
+++ FILES_to_INSTALL/includes/languages/japanese/modules/payment/lang.moneyorder.php
@@ -1,7 +1,7 @@
 <?php
 $define = [
     'MODULE_PAYMENT_MONEYORDER_TEXT_TITLE' => '小切手／マネーオーダー',
-    'MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION' => '顧客は支払いを郵送することができます。 注文確認メールでは、次のことを求められます。 <br><br>小切手またはマネーオーダーの支払い先は次のとおりです：<br>' . (defined('MODULE_PAYMENT_MONEYORDER_PAYTO') ? MODULE_PAYMENT_MONEYORDER_PAYTO : '<br>(あなたの店名)') . '<br><br>お支払いは次の宛先に郵送してください：<br>' . nl2br(STORE_NAME_ADDRESS) . '<br><br>' . 'ご注文は、支払いを受け取るまで発送されません。',
+    'MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION' => '顧客は支払いを郵送することができます。 注文確認メールでは、次のことを求められます。 <br><br>小切手またはマネーオーダーの支払い先は次のとおりです：<br>' . (defined('MODULE_PAYMENT_MONEYORDER_PAYTO') ? zen_config('MODULE_PAYMENT_MONEYORDER_PAYTO') : '<br>(あなたの店名)') . '<br><br>お支払いは次の宛先に郵送してください：<br>' . nl2br(zen_config('STORE_NAME_ADDRESS')) . '<br><br>' . 'ご注文は、支払いを受け取るまで発送されません。',
     'MODULE_PAYMENT_MONEYORDER_REMINDER' => '小切手または為替小切手の表面に、注文番号を必ずご記入ください。',
 // bof constant configuration titles and descriptions for payment module moneyorder
     'CFGTITLE_MODULE_PAYMENT_MONEYORDER_STATUS' => '小切手／マネーオーダーを有効にする',
@@ -17,7 +17,7 @@ $define = [
 // eof constant configuration titles and descriptions for payment module moneyorder
 ];
 if (defined('MODULE_PAYMENT_MONEYORDER_STATUS')) {
-    $define['MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER'] = "小切手またはマネーオーダーの支払い先は次のとおりです：\n\n" . MODULE_PAYMENT_MONEYORDER_PAYTO . "\n\nお支払いは次の宛先に郵送してください：\n" . STORE_NAME_ADDRESS . "\n\nご注文は、支払いを受け取るまで発送されません。";
+    $define['MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER'] = "小切手またはマネーオーダーの支払い先は次のとおりです：\n\n" . zen_config('MODULE_PAYMENT_MONEYORDER_PAYTO') . "\n\nお支払いは次の宛先に郵送してください：\n" . zen_config('STORE_NAME_ADDRESS') . "\n\nご注文は、支払いを受け取るまで発送されません。";
 }
 
 return $define;
