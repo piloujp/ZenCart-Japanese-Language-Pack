5c2301061b29f36c11a5cbcaefb603804aefcb8c
piloujp <117885587+piloujp@users.noreply.github.com>
2026-07-22 04:34:09 +0900

    Update plugin Edit Order to version 5.0.4

---
+++ optional_plugins/Encapsulated/EditOrders/v5.0.4/admin/includes/languages/japanese/extra_definitions/lang.edit_orders_extra_definitions.php
@@ -0,0 +1,13 @@
+<?php
+// -----
+// Admin-level extra language definitions for the "Edit Orders" plugin, provided by lat9.
+// Copyright (C) 2017-2024, Vinos de Frutas Tropicales.
+//
+// Last updated: EO v5.0.0
+//
+$define = [
+    'BOX_CONFIGURATION_EDIT_ORDERS' => '注文を編集',
+
+    'EO_ICON_DETAILS' => 'この注文を編集',
+];
+return $define;
