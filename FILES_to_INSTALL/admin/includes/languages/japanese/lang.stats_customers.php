6d8169866d57a44b50b2dc784fdce032bd2e0c4d
piloujp <117885587+piloujp@users.noreply.github.com>
2026-07-22 04:31:42 +0900

    Update to latest ZC files version

--- FILES_to_INSTALL/admin/includes/languages/japanese/lang.stats_customers.php
+++ FILES_to_INSTALL/admin/includes/languages/japanese/lang.stats_customers.php
@@ -3,12 +3,19 @@
  * @copyright Copyright 2003-2022 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Scott C Wilson 2022 Jan 11 New in v1.5.8-alpha $
+ * @version $Id: ZenExpert 2026-07-15 $
 */
 
 $define = [
     'HEADING_TITLE' => '顧客別の売上ランキング',
     'TABLE_HEADING_TOTAL_PURCHASED' => '総購入額',
+    'TEXT_DATE_RANGE_END_DATE' => '終了日:',
+    'TEXT_DATE_RANGE_START_DATE' => '開始日：',
+    'TEXT_DEFAULT_INTRO' => '顧客の購入合計額を表示するには、日付の範囲を指定するか、<strong>「全期間」</strong>をクリックしてください。',
+    'TEXT_RESULTS_ALL_TIME' => '全期間の統計結果',
+    'TEXT_RESULTS_RANGE' => '%s から %s までの結果',
+    'TEXT_SEARCH_ALL_TIME' => '最初から',
+    'TEXT_SEARCH_DATE_RANGE' => '検索範囲',
 ];
 
 return $define;
--- FILES_to_INSTALL/admin/includes/languages/japanese/lang.stats_products_purchased.php
+++ FILES_to_INSTALL/admin/includes/languages/japanese/lang.stats_products_purchased.php
@@ -3,7 +3,7 @@
  * @copyright Copyright 2003-2022 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Scott C Wilson 2022 Jan 11 New in v1.5.8-alpha $
+ * @version $Id: ZenExpert 2026-07-13 $
 */
 
 $define = [
@@ -14,6 +14,13 @@ $define = [
     'TABLE_HEADING_ORDERS_DATE_PURCHASED' => '日付',
     'TABLE_HEADING_CUSTOMERS_INFO' => '顧客情報',
     'TABLE_HEADING_PRODUCTS_QUANTITY' => '数量',
+    'TEXT_DATE_RANGE_END_DATE' => '終了日:',
+    'TEXT_DATE_RANGE_START_DATE' => '開始日：',
+    'TEXT_DEFAULT_INTRO' => '顧客の購入合計額を表示するには、日付の範囲を指定するか、<strong>「全期間」</strong>をクリックしてください。',
+    'TEXT_RESULTS_ALL_TIME' => '全期間の統計結果',
+    'TEXT_RESULTS_RANGE' => '%s から %s までの結果',
+    'TEXT_SEARCH_DATE_RANGE' => '検索範囲',
+    'TEXT_SEARCH_ALL_TIME' => '最初から',
 ];
 
 return $define;
