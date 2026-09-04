c6548d9c2dd46eb28a5118326586be577a52f037
piloujp <117885587+piloujp@users.noreply.github.com>
2026-09-05 00:21:30 +0900

    Added Database Structure Compare tool and Zx Seo Master
    Both v1.0.0

---
+++ optional_plugins/Encapsulated/DatabaseComp/v1.0.0/admin/includes/languages/japanese/lang.tool_database_comparison.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @copyright Copyright 2003-2026 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Piloujp 2026 Aug 21 New in v3.0.0-dev $
+ */
+
+$define = [
+    'HEADING_TITLE' => 'データベース構造比較ツール',
+    'TABLE_HEADING_DATABASE' => 'データベース',
+    'TABLE_HEADING_TABLE' => 'テーブル',
+    'TABLE_HEADING_COLUMN' => '名前',
+    'TABLE_HEADING_DEFAULT' => 'デフォルト値',
+    'TABLE_HEADING_NULLABLE' => 'NULL許容',
+    'TABLE_HEADING_COLLATION' => '照合順序',
+    'TABLE_HEADING_TYPE' => 'タイプ',
+    'TEXT_CHOOSE_DATABASE' => 'データベース（%s）を以下と比較してください：',
+    'TEXT_SELECT_AN_OPTION' => '選択してください...',
+    'TEXT_LEGEND' => '凡例：',
+    'TEXT_LEGEND_ONLY_LEFT' => 'データベース（%s）にのみ存在する列',
+    'TEXT_LEGEND_BOTH' => '両方のデータベースで差異がある列',
+    'TEXT_LEGEND_ONLY_RIGHT' => '２番目のデータベースにのみ存在する列（%s）',
+    'TEXT_TABLES_OWN' => 'データベースにのみ存在するテーブルが %s 個あります： ',
+    'TEXT_TABLES_OTHER' => '２番目のデータベースにのみ存在するテーブルが %s 個あります： ',
+    'TEXT_SEPARATOR' => ' ～ ',
+];
+
+return $define;
